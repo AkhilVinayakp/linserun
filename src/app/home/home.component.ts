3c190262b00f3d8946ac875ca32e83eb8ebe2884
Akhil Vinayak P <nirvanaosborn@gmail.com>
2019-12-04 14:16:49 +0530

    action list binding ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,12 @@ export class HomeComponent implements OnInit {
   btn={
      text:""
     }
+  val:any="tesrtrtrtr"
+  action:any
   constructor() { 
     this.btn.text="click here"
+    console.log("hai  ")
+    console.log(this.val)
   }
 
   ngOnInit(): void { }
