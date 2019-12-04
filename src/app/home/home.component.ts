d9c5b46ac1a4c0009477cdab9da11846b16a93ff
Akhil Vinayak P <nirvanaosborn@gmail.com>
2019-12-04 17:15:17 +0530

    layout for app prilimiary ok triggering of keypress ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,16 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   btn={
-     text:""
+     text:"click here"
     }
-  val:any="tesrtrtrtr"
-  action:any
+    linserun={
+      value:""
+    }
+action:string="Command";
   constructor() { 
-    this.btn.text="click here"
+    
     console.log("hai  ")
-    console.log(this.val)
   }
 
   ngOnInit(): void { }
 
+  //trigger when a change in the text box happens
+  performAct() :void{
+    //checking whether it is a search action or commmand running option is currently active
+    if(this.action=="Command")
+      {
+        this.executeShell_script()
+      }
+    else if(this.action=="search")
+    {
+        this.executeSearch_item()
+    }
+  }
+
+
+  executeShell_script()
+  {
+    console.log("trigger ok run commanf");
+  }
+
+
+  executeSearch_item()
+  {
+    console.log("trigger ok perform search");
+  }
 }
